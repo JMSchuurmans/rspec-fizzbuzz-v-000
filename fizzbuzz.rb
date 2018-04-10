29e04d99559364cd5808bbfb6e2118090f60526c
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-04-10 23:46:56 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz(int)
+  if int % 3 == 0
+    puts "Fizz"
+  end
+  if int % 5 == 0
+    puts "Buzz"
+  end
+  if int % 3 && 5
+    puts "FizzBuzz"
+  end
+  if int !% 3 && 5
+    puts "nil"
+  end
+end
